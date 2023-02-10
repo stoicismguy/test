e3dcd9218300d40538a4c3ebf404ed4d23341c89
stoicismguy <pyzan55@gmail.com>
2023-02-10 20:02:54 +0500

    some windows

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget
+from PyQt6.QtCore import QSize, Qt
 import sys
 
-app = QApplication(sys.argv)
 
-window = QWidget()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.label = QLabel()
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+app = QApplication(sys.argv)
+window = MainWindow()
 window.show()
 
 app.exec()
